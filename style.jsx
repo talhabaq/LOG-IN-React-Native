1877d84b388bd8651a228dd8f540886a5e0455f4
TALHA BAQAR HUSSAIN <159868699+talhabaq@users.noreply.github.com>
2024-07-28 23:07:10 +0500

    Create style.jsx

---
+++ style.jsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+const Exstyles=StyleSheet.create({
+    talha:
+        {fontSize:18,
+        color:'green',
+        borderWidth:2,
+        borderColor:"red",
+        margin:10}
+    
+})
+export default Exstyles
